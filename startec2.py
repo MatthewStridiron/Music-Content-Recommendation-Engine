d748875e3009144260180bba4ee41247ebd72e9b
MatthewStridiron <mstridiron.ptech@gmail.com>
2022-08-24 11:40:17 -0400

    Create startec2.py

---
+++ startec2.py
@@ -0,0 +1,10 @@
+import json
+import boto3
+region = 'us-east-1'
+instances = ['insert your instance id here']
+ec2 = boto3.client('ec2', region_name=region)
+
+def lambda_handler(event, context):
+    ec2.start_instances(InstanceIds=instances)
+    print('started your instances: ' + str(instances))
+    
